431f5c39dd468e4860ca2a2d53a83e2119f5a425
Tanvirazad3449 <tanvirazad49@gmail.com>
2026-06-16 00:45:44 +0200

    feat: add vocabulary dataset for Chapter 3 and update dataset labels

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.availableDatasets = [
-    { file: 'a11-1.json', label: 'A11-1' },
-    { file: 'a11-2.json', label: 'A11-2' },
+    { file: 'a11-1.json', label: 'A 1.1 - Chapter 1' },
+    { file: 'a11-2.json', label: 'A 1.1 - Chapter 2' },
+    { file: 'a11-3.json', label: 'A 1.1 - Chapter 3' },
+
 ];
